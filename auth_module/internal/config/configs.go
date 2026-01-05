94071af660b45a0233ad4bbff95e6ee6da74b897
reyana <222698448+rskisssz@users.noreply.github.com>
2026-01-05 23:45:40 +0300

    Добавлен пакет конфигураций с поддержкой переменных среды

---
+++ auth_module/internal/config/configs.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort string
+
+	// Время жизни
+	LoginTokenTTL   time.Duration // 5 минут
+	AccessTokenTTL  time.Duration // 1 минута
+	RefreshTokenTTL time.Duration // 7 дней
+	AuthCodeTTL     time.Duration // 1 минута
+
+	// Cleanup
+	CleanupInterval time.Duration // 1 минута
+}
+
+func Load() *Config {
+	_ = godotenv.Load()
+
+	return &Config{
+		ServerPort: getEnv("SERVER_PORT", "8081"),
+
+		LoginTokenTTL:   5 * time.Minute,    // 5 минут
+		AccessTokenTTL:  1 * time.Minute,    // 1 минута
+		RefreshTokenTTL: 7 * 24 * time.Hour, // 7 дней
+		AuthCodeTTL:     1 * time.Minute,    // 1 минута
+		CleanupInterval: 5 * time.Minute,
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
