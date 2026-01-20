c56548f53378e93556713a621fcf8a77439ced30
reyana <222698448+rskisssz@users.noreply.github.com>
2026-01-20 18:31:31 +0300

    fix

--- web_client_module/cmd/web/main.go
+++ web_client_module/cmd/web/main.go
@@ -1,58 +1,58 @@
 package main
 
 import (
- 	"html/template"
- 	"log"
- 	"net/http"
- 	"path/filepath"
- 	"time"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
 
- 	"web_client_module/internal/api"
- 	"web_client_module/internal/config"
- 	"web_client_module/internal/handlers"
- 	"web_client_module/internal/middleware"
- 	"web_client_module/internal/session"
+	"web_client_module/internal/api"
+	"web_client_module/internal/config"
+	"web_client_module/internal/handlers"
+	"web_client_module/internal/middleware"
+	"web_client_module/internal/session"
 )
 
 func main() {
- 	cfg := config.MustLoad()
+	cfg := config.MustLoad()
 
- 	tmpl, err := template.ParseGlob(filepath.Join("templates", "*.html"))
- 	if err != nil {
-  		log.Fatalf("Ошибка загрузки шаблонов: %v", err)
- 	}
+	tmpl, err := template.ParseGlob(filepath.Join("templates", "*.html"))
+	if err != nil {
+		log.Fatalf("Ошибка загрузки шаблонов: %v", err)
+	}
 
- 	store := session.NewRedisStore(cfg.RedisAddr, cfg.RedisPassword, cfg.RedisDB, cfg.SessionTTL)
+	store := session.NewRedisStore(cfg.RedisAddr, cfg.RedisPassword, cfg.RedisDB, cfg.SessionTTL)
 
- 	authClient := api.NewAuthClient(cfg.AuthURL, cfg.Mock)
- 	mainClient := api.NewMainClient(cfg.MainURL, cfg.Mock)
+	authClient := api.NewAuthClient(cfg.AuthURL, cfg.Mock)
+	mainClient := api.NewMainClient(cfg.MainURL, cfg.Mock)
 
- 	h := handlers.New(tmpl, store, authClient, mainClient, cfg)
+	h := handlers.New(tmpl, store, authClient, mainClient, cfg)
 
- 	mux := http.NewServeMux()
-	
- 	fs := http.FileServer(http.Dir("static"))
- 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux := http.NewServeMux()
 
- 	mux.HandleFunc("/", h.Home)
- 	mux.HandleFunc("/login", h.Login)
- 	mux.HandleFunc("/logout", h.Logout)
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
- 	mux.HandleFunc("/actions", h.ActionsPage)
- 	mux.HandleFunc("/actions/do", h.DoAction)
+	mux.HandleFunc("/", h.Home)
+	mux.HandleFunc("/login", h.Login)
+	mux.HandleFunc("/logout", h.Logout)
 
- 	var handler http.Handler = mux
- 	handler = middleware.Logging(handler)
- 	handler = middleware.SessionFlow(handler, store, authClient, cfg)
+	mux.HandleFunc("/actions", h.ActionsPage)
+	mux.HandleFunc("/actions/do", h.DoAction)
 
- 	srv := &http.Server{
-  		Addr:         ":" + cfg.Port,
-  		Handler:      handler,
-  		ReadTimeout:  10 * time.Second,
-  		WriteTimeout: 15 * time.Second,
-  		dleTimeout:  60 * time.Second,
- 	}
+	var handler http.Handler = mux
+	handler = middleware.Logging(handler)
+	handler = middleware.SessionFlow(handler, store, authClient, cfg)
 
- 	log.Printf("Web-client запущен: http://localhost:%s", cfg.Port)
- 	log.Fatal(srv.ListenAndServe())
+	srv := &http.Server{
+		Addr:         ":" + cfg.Port,
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	log.Printf("Web-client запущен: http://localhost:%s", cfg.Port)
+	log.Fatal(srv.ListenAndServe())
 }
--- web_client_module/go.mod
+++ web_client_module/go.mod
@@ -3,6 +3,11 @@ module web_client_module
 go 1.22
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/google/uuid v1.6.0
+	github.com/redis/go-redis/v9 v9.17.2
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
